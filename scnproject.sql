aac28ec02d322922ec31350cd27ebb2513c15bd0
SCNprojectMDM <40442466+SCNprojectMDM@users.noreply.github.com>
2018-06-21 10:51:27 +0200

    Add files via upload

---
+++ scnproject.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 21 jun 2018 om 10:56
+-- Serverversie: 10.1.26-MariaDB
+-- PHP-versie: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scnproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `rank` enum('Gebruiker','Donateur','VIP','Moderator','Administrator','Beheer') NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `rank`, `created_at`) VALUES
+(1, 'Mike', '$2y$10$jrJCuD/fRaxO/TTFL7QZyOdUoB0o2aBFTbb1vTyy8HF8QDg0xp6Ti', 'info@original-mike.com', 'Gebruiker', '2018-06-21 09:48:18');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ scnproject/upload.php
@@ -0,0 +1,104 @@
+<?php
+include_once('includes/bovenstuk.php');
+include_once('config.php');
+?>
+<div class="container">
+<?php if(isLoggedIn()): ?>
+<h2>Bestand Uploaden</h2>
+<form id="form_upload">
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <div class="form-group">
+            <label for="input-name">Bestands Naam</label>
+            <input type="text" name="name" class="form-control" id="input-name" value="">
+          </div>
+          <div class="form-group">
+            <label for="input-author">Auteur</label>
+            <input type="text" name="author" class="form-control" id="input-author" value="<?php echo htmlspecialchars($_SESSION['username']); ?> ">
+          </div>
+          <div class="form-group">
+            <label for="input-category">Categorie</label>
+            <select name="category_id" id="input-category" class="form-control select2-hidden-accessible" tabindex="-1" aria-hidden="true">
+              <option disabled="" selected="">Selecteer een categorie</option>
+                  <option data-slug="tools" value="1">Tools</option>
+                  <option data-slug="vehicles" value="2">Vehicles</option>
+                  <option data-slug="paintjobs" value="8">Aircrafts</option>
+                  <option data-slug="weapons" value="3">Codes</option>
+                  <option data-slug="scripts" value="4">Hacks</option>
+                  <option data-slug="player" value="5">Music</option>
+                  <option data-slug="maps" value="6">Pictures</option>
+                  <option data-slug="misc" value="7">NSFW</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="input-description">Beschrijving van het bestand</label>
+            <textarea name="description" id="input-description" class="form-control" rows="10" placeholder="Schrijf hier wat informatie over het bestand dat je gaat uploaden..."></textarea>
+            <p class="help-block">
+              <small>LET OP je kunt maar een paar HTML codes gebruiken: &lt;b&gt;, &lt;i&gt;, &lt;u&gt;, &lt;a&gt;, &lt;ul&gt;, &lt;ol&gt;, &lt;li&gt;</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="panel panel-default">
+        <div class="panel-body">
+          <label for="input-images">Foto's toevoegen</label> <small>(.jpg of .png - Maximaal 18, 2Mb per afbeelding.)</small>
+          <input type="file" name="images" id="input-images" accept="image/jpeg, image/png" multiple="">
+
+          <div class="image-list row ui-sortable">
+            <div class="template image-item col-xs-6 col-md-4 ui-sortable-handle" style="">
+            <span class="thumbnail">
+              <img class="img-responsive">
+            </span>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-success btn-sm">
+        Klaar, volgende stap.
+      </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </form>
+  <h4>Regels waar je je aan moet voldoen</h4>
+  <div class="well">
+              <p>
+                <strong>Houd je aan de onderstaande regels, doe je dit niet kunnen er maatregelen worden genomen.</strong>
+              </p>
+              <ul>
+                    <li>
+                      Bestanden die zijn toegestaan: .zip &amp; .rar
+                    </li>
+                    <li>
+                      Heb je geen toestemming om het bestand te uploaden, niet doen
+                    </li>
+                    <li>
+                      Geen pornografische dingen in de <i>niet</i> NSFW sectie
+                    </li>
+                    <li>
+                      Geen conflicten beginnen door bepaalde dingen in je beschrijving of wat dan ook te zetten
+                    </li>
+                    <li>
+                      Alle rechten gaan naar de rechtmatige eigenaar
+                    </li>
+                    <li>
+                      Heb je een bepaald iets van iemand anders in je file, zet hem dan ook in de beschrijving van je bestand
+                    </li>
+              </ul>
+            </div>
+</div>
+    <?php endif; ?>
+    <?php if(isNotLoggedIn()): ?>
+      <br>
+      <div class="well">
+        <p>Je moet ingelogd zijn om een bestand te kunnen uploaden.</p>
+        <p>Heb je al een account? <a href="login.php">Log dan hier in!</a></p>
+        <p>Heb je nog geen account? <a href="register.php">Maak dan hier GRATIS een account!</a></p>
+      </div>
+      <?php endif; ?>
+<?php 
+include_once('includes/onderstuk.php');
+?>
